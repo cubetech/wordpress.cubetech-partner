7b035b90dcae74eddf6dd3995aafe8a2c4b34e9f
james.schuepbach@cubetech.ch <james.schuepbach@cubetech.ch>
2014-05-19 13:52:41 +0200

    fixed timy mce 4.0 bug

--- cubetech-partner.php
+++ cubetech-partner.php
@@ -15,10 +15,21 @@ include_once('lib/cubetech-metabox.php');
 add_image_size( 'cubetech-partner-thumb', 152, 70 );
 
 wp_enqueue_script('jquery');
-wp_register_script('cubetech_partner_js', plugins_url('assets/js/cubetech-partner.js', __FILE__), 'jquery');
+wp_register_script('cubetech_partner_js', plugins_url('assets/js/cubetech-partner.js', __FILE__), array('jquery','wpdialogs'));
 wp_enqueue_script('cubetech_partner_js');
 
 add_action('wp_enqueue_scripts', 'cubetech_partner_add_styles');
+
+if(!function_exists('enqueue_css'))
+{
+	function enqueue_css()
+	{
+		wp_register_style('custom_jquery-ui-dialog', plugins_url('assets/css/jquery-ui-dialog.min.css', __FILE__) );
+		wp_enqueue_style('custom_jquery-ui-dialog');
+	}
+	add_action( 'admin_enqueue_scripts', 'enqueue_css' );
+} 
+
 
 function cubetech_partner_add_styles() {
 	wp_register_style('cubetech-partner-css', plugins_url('assets/css/cubetech-partner.css', __FILE__) );
